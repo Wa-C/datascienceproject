dde4903f400db3f2fd41d36fd47893835e6b7ee3
Syed Haque <wassi244255@gmail.com>
2024-10-21 11:06:31 +0200

    Model Trainer pipeline completed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.datascience import logger
 from src.datascience.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
 from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
 from src.datascience.pipeline.data_transformaiton_pipeline import DataTransformationTrainingPipeline
-
+from src.datascience.pipeline.model_trainer_pipeline import ModelTrainerTrainingPipeline
 #logger.info(" Welcome to custom loging")
 
 STAGE_NAME = " Data Ingestion Stage "
@@ -30,6 +30,16 @@ try:
     logger.info(f" >>>>>> stage {STAGE_NAME} started <<<<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.initiate_data_transformation()
+    logger.info(f" >>>stage { STAGE_NAME} completed <<<< \n\nx=======x")
+except Exception as e:
+        logger.exception(e)
+        raise e  
+
+STAGE_NAME = " Model Training Stage "
+try: 
+    logger.info(f" >>>>>> stage {STAGE_NAME} started <<<<<")
+    model_training = ModelTrainerTrainingPipeline()
+    model_training.initiate_model_training()
     logger.info(f" >>>stage { STAGE_NAME} completed <<<< \n\nx=======x")
 except Exception as e:
         logger.exception(e)
--- src/datascience/entity/config_entity.py
+++ src/datascience/entity/config_entity.py
@@ -20,3 +20,14 @@ class DataValidationConfig:
 class DataTransformationConfig:
     root_dir: Path
     data_path: Path    
+
+@dataclass
+class ModelTrainerConfig:
+    root_dir:Path
+    train_data_path:Path
+    test_data_path:Path
+    model_name: str
+    #Using Elastic net ML algorithm takes these parametes . Need to update the params.yaml file for it.
+    alpha: float
+    l1_ratio: float
+    target_column: str    
